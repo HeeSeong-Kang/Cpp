216264cb8657f586310c4d79833b081fe9d6ee46
HeeSeong-Kang <gmltjd7238@gmail.com>
2021-12-26 18:52:05 +0900

    Chapter1

---
+++ Chapter01/SimpleAdder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int val1;
+    cout << "First number: ";
+    cin >> val1;
+
+    int val2;
+    cout << "Second number: ";
+    cin >> val2;
+
+    int result = val1 + val2;
+    cout << "Result " << result << endl;
+
+    return 0;
+}
--- Chapter01/hellowold.cpp
+++ Chapter01/hellowold.cpp
@@ -6,6 +6,7 @@ int main(void)
     int num = 20;
     std::cout << num << std::endl;
     std::cout << "Hello World!" << std::endl;
+    cout << "Hello " << "World!" << endl;
 
     return 0;
 }
